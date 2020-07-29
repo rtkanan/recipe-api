def6a07373b035712d44e54db92db32baf104a1d
Kannan Nagarajan <=>
2020-07-30 00:43:10 +0530

    Update admin to support custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -41,6 +41,7 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
     email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, default='None')
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
