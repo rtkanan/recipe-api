e5047f4a65be0f59ba50728c98f92d711330a539
Kannan Nagarajan <=>
2020-07-29 23:14:22 +0530

    Add custom user model

---
+++ app/core/models.py
@@ -0,0 +1,76 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+# from django.conf import settings
+
+
+class UserManager(BaseUserManager):
+    """Manager for user profiles"""
+
+    def create_user(self, email, password=None, **kwargs):
+        """Create new user profile"""
+        if not email:
+            raise ValueError("User must have an email address")
+
+        # This makes the second half(domain) of the email lower case always
+        email = self.normalize_email(email)
+
+        user = self.model(email=email, **kwargs)
+        user.set_password(password)
+
+        # 'using' helps to support multiple databases
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create new superuser"""
+        user = self.create_user(email, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+
+        # 'using' helps to support multiple databases
+        user.save(using=self._db)
+
+        return user
+
+
+# Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    # Model Manager
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    # REQUIRED_FIELDS = ['name']
+
+#     def get_full_name(self):
+#         """Retrieve full name of the user"""
+#         return self.name
+
+#     def get_short_name(self):
+#         """Retrieve short name of the user"""
+#         return self.name
+
+#     def __str__(self):
+#         """Return string representation of the user"""
+#         return self.email
+
+# class ProfileFeedItem(models.Model):
+#     """Profile status update"""
+#     user_profile = models.ForeignKey(
+#         settings.AUTH_USER_MODEL,
+#         on_delete=models.CASCADE
+#     )
+#     status_text = models.CharField(max_length=255)
+#     created_on = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         """Return the model as a string"""
+#         return self.status_text
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,42 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful"""
+        email = "test@yopmail.com"
+        password = "Qwerty@123"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the new user email is normalized"""
+        email = 'test@YOPMAIL.COM'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password="Qwerty@123"
+        )
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, "Qwerty@123")
+
+    def test_create_super_user(self):
+        """Test creating a new super user"""
+        user = get_user_model().objects.create_superuser(
+            email='test@yopmail.com',
+            password="Qwerty@123"
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
